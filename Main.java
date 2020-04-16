dc02693981c30712ed3e4ea55eb3a33814179ed8
BenjaminCCross <benjamin.c.cross@gmail.com>
2020-04-16 11:58:49 -0400

    Massive facelift of Sorting and Searching to main

---
+++ Main.java
@@ -0,0 +1,37 @@
+import java.util.*;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class Main {
+    public static void main(String[] args){
+        int[] small = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        int[] big = new int[100];
+        SortingAndSearching s = new SortingAndSearching();
+
+        for (int i = 0; i < big.length; i++) {
+            big[i] = i;
+          }
+          System.out.println("Before Scramble");
+          print(small);
+          scramble(small);
+          System.out.println("After Scramble");
+          print(small);
+          System.out.println("After sort");
+          print(s.selectionSort(small));
+    }
+    public static void print(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+          System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+      }
+    
+      public static void scramble(int[] arr) {
+        Random rnd = ThreadLocalRandom.current();
+        for (int i = arr.length - 1; i > 0; i--) {
+          int index = rnd.nextInt(i + 1);
+          int a = arr[index];
+          arr[index] = arr[i];
+          arr[i] = a;
+        }
+      }
+}
---
+++ SortingAndSearching.java
@@ -0,0 +1,39 @@
+
+public class SortingAndSearching {
+  public SortingAndSearching() {
+  }
+
+  public int[] selectionSort(int[] arr) {
+    int n = arr.length;
+
+    for (int i = 0; i < n - 1; i++) {
+      int min_idx = i;
+      for (int j = i + 1; j < n; j++)
+        if (arr[j] < arr[min_idx])
+          min_idx = j;
+      int temp = arr[min_idx];
+      arr[min_idx] = arr[i];
+      arr[i] = temp;
+    }
+    return arr;
+  }
+
+  public int[] insertionSort(int[] arr) {
+    int n = arr.length;
+    for (int i = 1; i < n; ++i) {
+      int key = arr[i];
+      int j = i - 1;
+      while (j >= 0 && arr[j] > key) {
+        arr[j + 1] = arr[j];
+        j = j - 1;
+      }
+      arr[j + 1] = key;
+    }
+    return arr;
+  }
+
+  // todo: add MergeSort
+  // todo: add QuickSort
+  // todo: add HeapSort
+
+}
